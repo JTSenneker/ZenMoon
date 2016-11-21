5aaa69e2f435271969e75b99ba4eda7813e319e5
kaezen <haneryu@gmail.com>
2016-11-20 22:50:38 -0500

    got the grid spawn working and the targeting helper to track correctly. May need some fine tuning with the size of the collider. Still need to add different 'ground' types, but that should be easily doable

---
+++ Zen Moon/Assets/JDGroundSpawner.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JDGroundSpawner : MonoBehaviour {
+
+
+
+
+    public int mapWidth = 10;
+    public int mapHeight = 10;
+
+    public float tileXScale;
+    public float tileYScale;
+
+
+    int[,] map;
+
+    public GameObject ground;
+
+
+	// Use this for initialization
+	void Start () {
+        map = new int[mapHeight,mapWidth];
+        tileXScale = 1;
+        tileYScale = 1;
+
+        SpawnBasicTerrain();
+        InstantiateTerrain();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void SpawnBasicTerrain()
+    {
+        int t = 1;
+        for(int x = 0; x <= mapWidth-1; x++)
+        {
+            for(int y = 0; y <= mapHeight-1; y++)
+            {
+                map[x,y] = t;
+            }
+        }
+    }
+    void InstantiateTerrain()
+    {
+        GameObject tile;
+        for (int x = 0; x <= mapWidth - 1; x++)
+        {
+            for (int y = 0; y <= mapHeight - 1; y++)
+            {
+                Vector3 position = new Vector3(x * tileXScale, 0, y*tileYScale);
+                //tile = Instantiate(ground, position, Quaternion.identity) as GameObject;
+                tile = Instantiate(ground, position, Quaternion.Euler(90,0,0)) as GameObject;
+                //tile.transform.Rotate(new Vector3(1, 0, 0), 90);
+            }
+        }
+    }
+}
