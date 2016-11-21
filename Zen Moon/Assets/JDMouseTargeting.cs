d8c85ea8b8f8d545906dfa54606114b67fcffd1b
kaezen <haneryu@gmail.com>
2016-11-20 22:50:38 -0500

    got the grid spawn working and the targeting helper to track correctly. May need some fine tuning with the size of the collider. Still need to add different 'ground' types, but that should be easily doable

---
+++ Zen Moon/Assets/JDMouseTargeting.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JDMouseTargeting : MonoBehaviour {
+
+    public GameObject mouseHelper;
+    Vector3 basePosition;
+
+	// Use this for initialization
+	void Start () {
+
+        basePosition = new Vector3(5, -5, 5);
+
+        //create the helper object, spawn it below the board right now
+        mouseHelper = Instantiate(mouseHelper, basePosition, Quaternion.identity) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+
+        Physics.Raycast(ray, out hit);
+        Debug.DrawRay(ray.origin, ray.direction, Color.blue,5);
+
+        if (hit.collider != null)
+        {
+            //mouseHelper.transform.position = hit.collider.transform.position;
+            mouseHelper.transform.position = new Vector3(hit.collider.transform.position.x, 0, hit.collider.transform.position.z);
+            //print("helper " + mouseHelper.transform.position);
+            //print(hit.collider.name);
+            //print(hit.collider.transform.position);
+        }
+        else transform.position = basePosition;
+
+
+	}
+}
---
+++ Zen Moon/Assets/scripts/JDTileEnum.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public  enum tiles
+{
+    dirt = 1,
+    tilledDirt = 2,
+    rock = 3
+}
+
+public class JDTileEnum : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
