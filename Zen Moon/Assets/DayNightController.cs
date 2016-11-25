5ec138e673454d642e85ab716141a8d90e76356a
asimpson2013 <simpsoa6@ferris.edu>
2016-11-25 16:31:21 -0500

    Day Night Cycle has been added

---
+++ Zen Moon/Assets/DayNightController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Controlls the day and night cycle
+/// </summary>
+public class DayNightController : MonoBehaviour
+{
+    /// <summary>
+    /// The time the day lasts in minutes
+    /// </summary>
+    public int dayTime = 5;
+    /// <summary>
+    /// The time the night lasts in minutes
+    /// </summary>
+    public int nightTime = 3;
+    /// <summary>
+    /// How many seconds are in a minute
+    /// </summary>
+    float secsInMin = 60;
+    /// <summary>
+    /// The Timer that counts down the minutes
+    /// </summary>
+    float Timer;
+    /// <summary>
+    /// Whether or not it is day time
+    /// </summary>
+    bool isDay = true;
+
+    /// <summary>
+    /// Initializes the timer for day time
+    /// </summary>
+    void Start()
+    {
+        Timer = dayTime * secsInMin;
+    }
+
+    /// <summary>
+    /// Counts the timerdown and resets the timer to the night or day time cycle depending on whether or not it was just day time.
+    /// </summary>
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+
+        if (Timer <= 0)
+        {
+            if (isDay)
+            {
+                Timer = nightTime * secsInMin;
+                isDay = false;
+            }
+            else if (!isDay)
+            {
+                Timer = dayTime * secsInMin;
+                isDay = true;
+            }
+        }
+    }
+}
