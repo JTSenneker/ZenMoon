704bf959c2cbf0e1b0c36db9be6ffa324fb1eb17
kaezen <haneryu@gmail.com>
2016-11-29 22:55:36 -0500

    added player inventory, ability to plant seeds, water tiles (currently no logic with plants), plants grow as the day changes and bloom at a set time. need to add in ability to harvest plants, resetting some and getting paid for it (when day completes). also still need to figure out why it takes 2 clicks to till ground, not just one

---
+++ Zen Moon/Assets/JDPlantClass.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JDPlantClass : MonoBehaviour {
+
+    //how long it grows for
+    public int growthTime;
+    //when it was planted
+    int plantedTime;
+    //bloomed means the plant can be harvested
+    public bool bloomed;
+    //has it been harvested yet?
+    public bool harvested = false;
+    //does it stick around after being harvested?
+    public bool destroyOnHarvest = true;
+    //how much money is it worth?
+    public int moneyValue;
+    //the tile this plant is 'planted' on
+    public GameObject plantedTile;
+
+
+	// Use this for initialization
+	void Start () {
+        plantedTime = JDStaticVariables.dayCount;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //this is where we update the sprite according to it's status
+        //unless we use the animator for that
+
+        //this is where we check it's planted time, compared to it's growth time
+
+
+   ///////still need to put in logic for utilizing the watered ground
+   //most likely, if the ground from the planted tile is not watered, add 1 to planted time, so time effectively doesn't pass
+   //for this plant
+
+        if (JDStaticVariables.dayCount == plantedTime + growthTime && !bloomed)
+        {
+            bloomed = true;
+            harvested = false;
+            transform.localScale *= 2;
+        }
+        
+        
+        //once bloomed, we can then harvest from it
+        //some plants stick around after being harvested
+        //so if they have been harvested, they don't change until they bloom again
+    }
+
+    public void Harvest()
+    {
+        if (destroyOnHarvest)
+        {
+            Destroy(gameObject);
+        }else
+        {
+            harvested = true;
+            plantedTime = JDStaticVariables.dayCount;
+        }
+    }
+}
---
+++ Zen Moon/Assets/scripts/JDStaticVariables.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class JDStaticVariables
+{
+
+    public enum tiles
+    {
+        dirt = 1,
+        tilledDirt = 2,
+        rock = 3,
+        seeded = 4,
+        watered = 5,
+        //waterSeeds = 6
+    }
+
+    public enum inventory
+    {
+        nothing = 0,
+        water = 1,
+        //seed = 2,
+        rice = 3,
+        corn = 4,
+        daikon = 5,
+        leek = 6,
+        scythe,
+        hoe
+    }
+
+    public static int riceGrowthTime = 4;
+    public static int riceMoneyValue = 50;
+
+    public static int cornGrowthTime = 7;
+    public static int cornMoneyValue = 100;
+
+    public static int leekGrowthTime = 3;
+    public static int leekMoneyValue = 75;
+
+    public static int daikonGrowthTime = 5;
+    public static int daikonMoneyValue = 100;
+
+
+    //what is currently selected, defaults to nothing
+    public static inventory playerInventory = inventory.water;
+
+    //to check if we're using the mouse or gamepad, defaults to mouse
+    public static bool usingMouse = true;
+
+    //keep track of the number of days, starts at 1, because not an array
+    public static int dayCount = 1;
+    //total amount of zen the player has
+    public static int zenTotal;
+    //total amount of money the player has
+    public static int moneyTotal;
+
+    static public void DayChange()
+    {
+        dayCount++;
+
+        //put in logic here that makes all watered tiles turn back to seeded
+    }
+}
