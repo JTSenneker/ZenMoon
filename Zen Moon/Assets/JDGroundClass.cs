d00e3d05069b5739e7e4402d1edd090f305f6dec
kaezen <haneryu@gmail.com>
2016-12-01 12:18:04 -0500

    added ability to harvest crops, ability to store items in the shipping box, when the day changes over, the box is emptied and the player is paid for the stuff that was in the box, still need to program water check for plant to grow and water logic + SEEDED

--- Zen Moon/Assets/JDGroundClass.cs
+++ Zen Moon/Assets/JDGroundClass.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class JDGroundClass : MonoBehaviour {
 
+    //the current status of this tile, tilled, seeded, etc
    public JDStaticVariables.tiles _tileStatus;
+    //what this tile has placed/planted ontop of it
+    public GameObject occupiedWith;
 
 	// Use this for initialization
 	void Start () {
--- Zen Moon/Assets/JDTargetingInteractions.cs
+++ Zen Moon/Assets/JDTargetingInteractions.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class JDTargetingInteractions : MonoBehaviour {
+public class JDTargetingInteractions : MonoBehaviour
+{
 
     public GameObject target;
     public GameObject rice;
@@ -9,16 +10,20 @@ public class JDTargetingInteractions : MonoBehaviour {
     public GameObject leek;
     public GameObject daikon;
 
+    public GameObject shippingBox;
+
     JDStaticVariables.tiles targetStatus;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
     //syntax: tile status check, then player inventory
-	void Update () {
+    void Update()
+    {
 
         if (Input.GetButtonDown("Fire1"))
         {
@@ -27,95 +32,200 @@ public class JDTargetingInteractions : MonoBehaviour {
                 target = JDMouseTargeting.target;
                 if (target != null)
                 {
-                    targetStatus = target.GetComponentInChildren<JDGroundClass>()._tileStatus;
+                    if (target.tag == "Ground")
+                    {
+                        targetStatus = target.GetComponentInChildren<JDGroundClass>()._tileStatus;
 
-                    //if planting crops
+                        //if planting crops
 
-  /*                  if((JDStaticVariables.playerInventory == JDStaticVariables.inventory.corn) || 
-                       (JDStaticVariables.playerInventory == JDStaticVariables.inventory.leek) ||
-                       (JDStaticVariables.playerInventory == JDStaticVariables.inventory.rice) ||
-                       (JDStaticVariables.playerInventory == JDStaticVariables.inventory.daikon))
-                       {
-                        //if the target is tilled
+                        /*                  if((JDStaticVariables.playerInventory == JDStaticVariables.inventory.cornSeed) || 
+                                             (JDStaticVariables.playerInventory == JDStaticVariables.inventory.leek) ||
+                                             (JDStaticVariables.playerInventory == JDStaticVariables.inventory.riceSeed) ||
+                                             (JDStaticVariables.playerInventory == JDStaticVariables.inventory.daikonSeed))
+                                             {
+                                              //if the target is tilled
+                                              if (targetStatus == JDStaticVariables.tiles.tilledDirt)
+                                              {
+                                                  target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.seeded;
+                                              }
+                                          }
+                                          */
+                        //if tilled dirt
                         if (targetStatus == JDStaticVariables.tiles.tilledDirt)
                         {
-                            target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.seeded;
-                        }
-                    }
-                    */
-                    //if tilled dirt
-                    if (targetStatus == JDStaticVariables.tiles.tilledDirt)
-                    {
-                        //create plant object, since we're going to plant something
-                        GameObject plant;
-                        //if planting rice
-                        if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.rice)
-                        {
-                            target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.seeded;
-                            plant = Instantiate(rice, target.transform.position, Quaternion.identity) as GameObject;
-                            plant.GetComponentInChildren<JDPlantClass>().growthTime = JDStaticVariables.riceGrowthTime;
-                            plant.GetComponentInChildren<JDPlantClass>().moneyValue = JDStaticVariables.riceMoneyValue;
-                            plant.GetComponentInChildren<JDPlantClass>().destroyOnHarvest = false;
-                        }
+                            //create plant object, since we're going to plant something
+                            GameObject plant;
+                            //if planting riceSeed
+                            if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.riceSeed)
+                            {
+                                //set the tile as 'seeded'
+                                target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.seeded;
+                                //create the plant object
+                                plant = Instantiate(rice, target.transform.position, Quaternion.identity) as GameObject;
+                                //set how long the plant needs to grow
+                                plant.GetComponentInChildren<JDPlantClass>().growthTime = JDStaticVariables.riceGrowthTime;
+                                //set how much the plant is worth
+                                plant.GetComponentInChildren<JDPlantClass>().moneyValue = JDStaticVariables.riceMoneyValue;
+                                //tell the tile that it is occupied with something
+                                target.GetComponentInChildren<JDGroundClass>().occupiedWith = plant;
+                                //set whether the plant sticks around after it's harvested
+                                plant.GetComponentInChildren<JDPlantClass>().destroyOnHarvest = false;
+                            }
 
-                        //if planting leek
-                        if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.leek)
-                        {
-                            target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.seeded;
-                            plant = Instantiate(leek, target.transform.position, Quaternion.identity) as GameObject;
-                            plant.GetComponentInChildren<JDPlantClass>().growthTime = JDStaticVariables.leekGrowthTime;
-                            plant.GetComponentInChildren<JDPlantClass>().moneyValue = JDStaticVariables.leekMoneyValue;
-                        }
+                            //if planting leek
+                            if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.leekSeed)
+                            {
+                                target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.seeded;
+                                plant = Instantiate(leek, target.transform.position, Quaternion.identity) as GameObject;
+                                plant.GetComponentInChildren<JDPlantClass>().growthTime = JDStaticVariables.leekGrowthTime;
+                                plant.GetComponentInChildren<JDPlantClass>().moneyValue = JDStaticVariables.leekMoneyValue;
+                                target.GetComponentInChildren<JDGroundClass>().occupiedWith = plant;
+                            }
 
-                        //if planting corn
-                        if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.corn)
-                        {
-                            target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.seeded;
-                            plant = Instantiate(corn, target.transform.position, Quaternion.identity) as GameObject;
-                            plant.GetComponentInChildren<JDPlantClass>().growthTime = JDStaticVariables.cornGrowthTime;
-                            plant.GetComponentInChildren<JDPlantClass>().moneyValue = JDStaticVariables.cornMoneyValue;
-                            plant.GetComponentInChildren<JDPlantClass>().destroyOnHarvest = false;
-                        }        
+                            //if planting cornSeed
+                            if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.cornSeed)
+                            {
+                                target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.seeded;
+                                plant = Instantiate(corn, target.transform.position, Quaternion.identity) as GameObject;
+                                plant.GetComponentInChildren<JDPlantClass>().growthTime = JDStaticVariables.cornGrowthTime;
+                                plant.GetComponentInChildren<JDPlantClass>().moneyValue = JDStaticVariables.cornMoneyValue;
+                                target.GetComponentInChildren<JDGroundClass>().occupiedWith = plant;
+                                plant.GetComponentInChildren<JDPlantClass>().destroyOnHarvest = false;
+                            }
 
-                        //if planting daikon
-                        if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.daikon)
+                            //if planting daikonSeed
+                            if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.daikonSeed)
+                            {
+                                target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.seeded;
+                                plant = Instantiate(daikon, target.transform.position, Quaternion.identity) as GameObject;
+                                plant.GetComponentInChildren<JDPlantClass>().growthTime = JDStaticVariables.daikonGrowthTime;
+                                plant.GetComponentInChildren<JDPlantClass>().moneyValue = JDStaticVariables.daikonMoneyValue;
+                                target.GetComponentInChildren<JDGroundClass>().occupiedWith = plant;
+                            }
+                        }//end of tilledDirtcheck
+
+                        if (targetStatus == JDStaticVariables.tiles.seeded)
                         {
-                            target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.seeded;
-                            plant = Instantiate(daikon, target.transform.position, Quaternion.identity) as GameObject;
-                            plant.GetComponentInChildren<JDPlantClass>().growthTime = JDStaticVariables.daikonGrowthTime;
-                            plant.GetComponentInChildren<JDPlantClass>().moneyValue = JDStaticVariables.daikonMoneyValue;
-                        }
-                    }
+                            //print("tryingtoharvest");
+                            if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.scythe)
+                            {
+                                // print("usingScythe");
+                                // print((target.gameObject.GetComponentInChildren<JDGroundClass>().occupiedWith == rice));
+                                // print(target.gameObject.GetComponentInChildren<JDGroundClass>().occupiedWith.name == "Rice(Clone)");
+                                //if (target.gameObject.GetComponentInChildren<JDGroundClass>().occupiedWith == rice)
+
+                                //if harvesting rice
+                                if (target.gameObject.GetComponentInChildren<JDGroundClass>().occupiedWith.name == rice.name + "(Clone)")
+                                {
+                                    //print("GOT RICE?");
+                                    //check if the plant can be harvested
+                                    if (target.GetComponentInChildren<JDGroundClass>().occupiedWith.GetComponentInChildren<JDPlantClass>().CanHarvest())
+                                    {
+                                        //run the planted plant's 'harvest' function'
+                                        target.GetComponentInChildren<JDGroundClass>().occupiedWith.GetComponentInChildren<JDPlantClass>().Harvest();
+
+                                        //load the plant into the player's inventory
+                                        JDStaticVariables.playerInventory = JDStaticVariables.inventory.ricePlant;
+                                        print("harvested rice");
+                                    }
+                                }
+                                //if harvesting leek
+                                if (target.gameObject.GetComponentInChildren<JDGroundClass>().occupiedWith.name == leek.name + "(Clone)")
+                                {
+                                    //print("GOT RICE?");
+                                    //check if the plant can be harvested
+                                    if (target.GetComponentInChildren<JDGroundClass>().occupiedWith.GetComponentInChildren<JDPlantClass>().CanHarvest())
+                                    {
+                                        //run the planted plant's 'harvest' function'
+                                        target.GetComponentInChildren<JDGroundClass>().occupiedWith.GetComponentInChildren<JDPlantClass>().Harvest();
+
+                                        //load the plant into the player's inventory
+                                        JDStaticVariables.playerInventory = JDStaticVariables.inventory.leekPlant;
+                                        print("harvested leek");
+                                    }
+                                }
+                                //if harvesting daikon
+                                if (target.gameObject.GetComponentInChildren<JDGroundClass>().occupiedWith.name == daikon.name + "(Clone)")
+                                {
+                                    //print("GOT RICE?");
+                                    //check if the plant can be harvested
+                                    if (target.GetComponentInChildren<JDGroundClass>().occupiedWith.GetComponentInChildren<JDPlantClass>().CanHarvest())
+                                    {
+                                        //run the planted plant's 'harvest' function'
+                                        target.GetComponentInChildren<JDGroundClass>().occupiedWith.GetComponentInChildren<JDPlantClass>().Harvest();
+
+                                        //load the plant into the player's inventory
+                                        JDStaticVariables.playerInventory = JDStaticVariables.inventory.daikonPlant;
+                                        print("harvested daikon");
+                                    }
+                                }
+                                //if harvesting corn
+                                if (target.gameObject.GetComponentInChildren<JDGroundClass>().occupiedWith.name == corn.name + "(Clone)")
+                                {
+                                    //print("GOT RICE?");
+                                    //check if the plant can be harvested
+                                    if (target.GetComponentInChildren<JDGroundClass>().occupiedWith.GetComponentInChildren<JDPlantClass>().CanHarvest())
+                                    {
+                                        //run the planted plant's 'harvest' function'
+                                        target.GetComponentInChildren<JDGroundClass>().occupiedWith.GetComponentInChildren<JDPlantClass>().Harvest();
+
+                                        //load the plant into the player's inventory
+                                        JDStaticVariables.playerInventory = JDStaticVariables.inventory.cornPlant;
+                                        print("harvested corn");
+                                    }
+                                }
+                            }//end of scythe check
+                        }//end of seeded check
 
 
                         //base dirt
                         if (targetStatus == JDStaticVariables.tiles.dirt)
-                    {
-                        //if holding hoe
-                        if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.hoe)
                         {
-                            target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.tilledDirt;
-                        }
-                    }
-
-
-
-
-                    //watering seeded ground
-                    if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.water)
-                    {
-                        //can only water seeded ground
-                        if (targetStatus == JDStaticVariables.tiles.seeded)
-                        {
-                            target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.watered;
+                            //if holding hoe
+                            if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.hoe)
+                            {
+                                target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.tilledDirt;
+                            }
                         }
 
+
+
+
+                        //watering seeded ground
+                        if (JDStaticVariables.playerInventory == JDStaticVariables.inventory.water)
+                        {
+                            //can only water seeded ground
+                            if (targetStatus == JDStaticVariables.tiles.seeded)
+                            {
+                                target.GetComponentInChildren<JDGroundClass>()._tileStatus = JDStaticVariables.tiles.watered;
+                            }
+
+                        }
+                        print(targetStatus);
+                    }//end of ground check
+
+                    if(target.gameObject == shippingBox)
+                    {
+                        if(JDStaticVariables.playerInventory == JDStaticVariables.inventory.ricePlant)
+                        {
+                            JDStaticVariables.shippingStock.Add(rice);
+                        }
+                        if(JDStaticVariables.playerInventory == JDStaticVariables.inventory.leekPlant)
+                        {
+                            JDStaticVariables.shippingStock.Add(leek);
+                        }
+                        if(JDStaticVariables.playerInventory == JDStaticVariables.inventory.daikonPlant)
+                        {
+                            JDStaticVariables.shippingStock.Add(daikon);
+                        }
+                        if(JDStaticVariables.playerInventory == JDStaticVariables.inventory.cornPlant)
+                        {
+                            JDStaticVariables.shippingStock.Add(corn);
+                        }
+                        //print("Shipping stuff");
                     }
-
-
-                    print(targetStatus);
                 }//end of null check
             }
         }
-	}
+    }
 }
